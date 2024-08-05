9770bbb13780b466672e2508f77f4d9be0aa9d8b
marisv <maris.veismanis@meawallet.com>
2024-08-05 14:13:17 +0300

    Empty encrypted data generator activity

--- app/src/main/java/com/meawallet/mtp/sampleapp/ui/addcard/InitWithEncryptedPanFragment.kt
+++ app/src/main/java/com/meawallet/mtp/sampleapp/ui/addcard/InitWithEncryptedPanFragment.kt
@@ -1,14 +1,22 @@
 package com.meawallet.mtp.sampleapp.ui.addcard
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.meawallet.mtp.MeaInitializeDigitizationParameters
 import com.meawallet.mtp.sampleapp.R
+import com.meawallet.mtp.sampleapp.dto.EncryptedData
+import com.meawallet.mtp.sampleapp.helpers.AlertDialogHelper
 import com.meawallet.mtp.sampleapp.utils.EncryptedDataReader
 
 class InitWithEncryptedPanFragment : DigitizationParamProviderFragment() {
@@ -30,6 +38,28 @@ class InitWithEncryptedPanFragment : DigitizationParamProviderFragment() {
     private lateinit var dynamicDataGetBtn: MaterialButton
 
     private lateinit var initializeViewSet: MutableSet<View>
+
+    private val encryptedCardDataResult : ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult(), ActivityResultCallback {
+                result: ActivityResult ->
+            if(result.resultCode == RESULT_OK) {
+                val data = result.data
+
+                val encryptedCardData = data?.getStringExtra(EncryptedDataGeneratorActivity.ENCRYPTED_CARD_DATA) ?: ""
+                val publicKeyFingerprint = data?.getStringExtra(EncryptedDataGeneratorActivity.PUBLIC_KEY_FINGERPRINT) ?: ""
+                val encryptedKey = data?.getStringExtra(EncryptedDataGeneratorActivity.ENCRYPTED_KEY) ?: ""
+                val initialVector = data?.getStringExtra(EncryptedDataGeneratorActivity.IV) ?: ""
+
+                if (encryptedCardData.isNotEmpty()) {  // if data not empty
+                    val encryptedData = EncryptedData(encryptedCardData, publicKeyFingerprint, encryptedKey, initialVector)
+                    fillEncryptedData(encryptedData)
+                } else {
+                    showDataGenerationFailedDialog()  // don't do nothing, display popup
+                }
+            } else {
+                showDataGenerationFailedDialog()  // don't do nothing, display popup
+            }
+        })
 
 
     override fun onCreateView(
@@ -65,15 +95,33 @@ class InitWithEncryptedPanFragment : DigitizationParamProviderFragment() {
             readAndFillStaticData()
         }
 
+        dynamicDataGetBtn.setOnClickListener {
+            generateAndFillDynamicData()
+        }
+
     }
 
     private fun readAndFillStaticData(){
         val encryptedData = context?.let { EncryptedDataReader.readEncryptedData(it) }
 
-        encryptedCardDataInput.setText(encryptedData?.encryptedCardData)
-        publicKeyFingerprintInput.setText(encryptedData?.publicKeyFingerprint)
-        encryptedKeyInput.setText(encryptedData?.encryptedKey)
-        initialVectorInput.setText(encryptedData?.iv)
+        encryptedData?.let { fillEncryptedData(it) }
+    }
+
+    private fun generateAndFillDynamicData(){
+        val getEncryptedDataIntent = Intent(activity, EncryptedDataGeneratorActivity::class.java)
+        encryptedCardDataResult.launch(getEncryptedDataIntent)
+    }
+
+    private fun fillEncryptedData(encryptedData: EncryptedData) {
+        encryptedCardDataInput.setText(encryptedData.encryptedCardData)
+        publicKeyFingerprintInput.setText(encryptedData.publicKeyFingerprint)
+        encryptedKeyInput.setText(encryptedData.encryptedKey)
+        initialVectorInput.setText(encryptedData.iv)
+    }
+
+    private fun showDataGenerationFailedDialog() {
+        activity?.let {
+            AlertDialogHelper.showErrorMessageDialog(it, "Encrypted data generation failed") }
     }
 
     override fun isInputValid(): Boolean {
